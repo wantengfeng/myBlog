dc4d089ebab3ff4a61a05042ede9d52ae151023d
jameswan <jameswan@tencent.com>
2021-05-03 16:00:14 +0800

    myBlog

---
+++ docs/.vuepress/config/nav/index.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { text: 'Home', link: '/', icon: 'reco-home' },
+  { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+  { text: 'GitHub', link: 'https://github.com/wantengfeng', icon: 'reco-github' }
+]
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+---
+home: true
+bgImage: '/banner.jpg'
+bgImageStyle: {
+  height: '450px'
+}
+faceImage: '/head.png'
+---
